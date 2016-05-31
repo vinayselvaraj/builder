62f5929d3fb799ad73f846e730de4c3206c7963b
Vinay Selvaraj <vinay@selvaraj.com>
2016-05-31 14:51:42 -0400

    Switched to centos

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
-FROM alpine
+FROM centos:7
 MAINTAINER Vinay Selvaraj <vinay@selvaraj.com>
 
-RUN apk add --update    \
-            python      \
-            py-pip      \
-            make
+RUN yum -y install python docker
 
+# Install PIP
+RUN curl "https://bootstrap.pypa.io/get-pip.py" -o "/tmp/get-pip.py"
+RUN python2.7 /tmp/get-pip.py
 RUN pip2.7 install --upgrade pip
+
+# Install AWS CLI
 RUN pip2.7 install awscli
 
 RUN mkdir -p /opt/builder/bin
--- builder.sh
+++ builder.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
 
-# Place holder
+set -xe
+
+# Check for required environment variables
+if [ -z INPUT_S3_BUCKET ]
+then
+    echo "INPUT_S3_BUCKET is not set"
+    exit 1
+fi
+
+if [ -z INPUT_S3_OBJECT_KEY ]
+then
+    echo "INPUT_S3_OBJECT_KEY is not set"
+    exit 1
+fi
+
+mkdir -p /opt/builder/tmp
+mkdir -p /opt/builder/workspace
+
+aws s3 cp s3://$INPUT_S3_BUCKET/$INPUT_S3_OBJECT_KEY /opt/builder/tmp
+unzip /opt/builder/tmp/$INPUT_S3_OBJECT_KEY -d /opt/builder/workspace
+
+/opt/builder/workspace/build.sh
