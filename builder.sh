b74ea87c5a9307a061311dbb1adbb805505e78a6
Vinay Selvaraj <vinay@selvaraj.com>
2016-06-02 15:40:56 -0400

    Changed order of publishing build tag to output location

--- builder.sh
+++ builder.sh
@@ -27,10 +27,6 @@ then
     exit 1
 fi
 
-# Store creds
-CP_AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-CP_AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-CP_AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN
 
 
 # Install dependencies
@@ -45,6 +41,14 @@ mkdir -p $WORKSPACE
 # Enable SigV4
 aws configure set s3.signature_version s3v4
 aws s3 cp s3://$INPUT_S3_BUCKET/$INPUT_S3_OBJECT_KEY $BUILDER_HOME/tmp/source.zip
+
+# Set image name
+IMAGE_NAME=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
+
+# Send image name to build output location
+TMPFILE=`mktemp`
+echo $IMAGE_NAME > $TMPFILE
+aws s3 cp $TMPFILE s3://$OUTPUT_S3_BUCKET/$OUTPUT_S3_OBJECT_KEY
 
 # Unset CodePipeline creds
 unset AWS_ACCESS_KEY_ID
@@ -66,19 +70,8 @@ then
     exit 1
 fi
 
-IMAGE_NAME=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
-
 docker build -t $IMAGE_NAME .
 $(aws ecr get-login --region $AWS_DEFAULT_REGION)
 docker push $IMAGE_NAME
-
-# Set CodePipeline creds
-export AWS_ACCESS_KEY_ID=$CP_AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$CP_AWS_SECRET_ACCESS_KEY
-export AWS_SESSION_TOKEN=$CP_AWS_SESSION_TOKEN
-
-TMPFILE=`mktemp`
-echo $IMAGE_NAME > $TMPFILE
-aws s3 cp $TMPFILE s3://$OUTPUT_S3_BUCKET/$OUTPUT_S3_OBJECT_KEY
 
 echo "Build complete.."
